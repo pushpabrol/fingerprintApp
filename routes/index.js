07acb05915a959ab4f7a7261c9e7de5906a2f8cf
pushpabrol <pushp.abrol@gmail.com>
2023-09-07 09:16:16 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,33 @@
+var router = require('express').Router();
+const { requiresAuth } = require('express-openid-connect');
+
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Auth0 Webapp sample Nodejs',
+    isAuthenticated: req.oidc.isAuthenticated()
+  });
+});
+
+router.get('/profile', requiresAuth(), function (req, res, next) {
+  res.render('profile', {
+    userProfile: JSON.stringify(req.oidc.user, null, 2),
+    title: 'Profile page'
+  });
+});
+
+
+router.get('/login', function (req, res, next) {
+  const visitorId = req.query.visitorId;
+  const requestId = req.query.requestId;
+
+  res.oidc.login({
+    returnTo: '/profile',
+    authorizationParams: {
+      redirect_uri: 'http://localhost:3000/callback',
+      visitorId,
+      requestId
+    },
+  })
+});
+
+module.exports = router;
